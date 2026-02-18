2c328d0d4ef1bbd0566a0c3505ca2b7c05f7a3f6
vikrammjeett <vikramjeet.singh@2sfg.com>
2026-02-18 10:54:46 +0000

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,36 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+if "step" not in st.session_state:
+    st.session_state.step = "stop_critical_machine_check"
+st.write("Current step:", st.session_state.step)
+
+#decision logic
+if st.session_state.step == "stop_critical_machine_check":
+    st.write("Does the component failure stop a critical machine?")
+
+
+    if st.button("Yes"):
+        st.session_state.step = "Critical"
+    
+    if st.button("No"):
+        st.session_state.step = "Inhibit_safety_check"
+
+elif st.session_state.step == "Inhibit_safety_check":
+    st.write("Does it inhibit a safety system?")
+
+    if st.button("Yes"):
+        st.session_state.step = "Critical"
+
+    if st.button("No"):
+        st.session_state.step = "Stop_production_check"
+elif st.session_state.step == "Stop_production_check":
+    st.write("Does it stop a production line?")
+    if st.button("Yes"):
+        st.session_state.step = "critical"
+    if st.button("No"):
+        st.session_state.step = "not_critical"
+elif st.session_state.step == "critical":
+    st.info("Component is critical")
+
+elif st.session_state.step == "not_critical":
+    st.info("Component is NOT critical.")
